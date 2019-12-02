37b954678b52c9437e14cdd4591735549875bf40
Joel Walker <11062267+JoelW16@users.noreply.github.com>
2019-12-02 11:36:19 +0000

    added relative paths for reading data

--- DayOne/ElfFuelRequirements.cs
+++ DayOne/ElfFuelRequirements.cs
@@ -3,14 +3,14 @@ using System.IO;
 using System.Linq;
 
 namespace DayOne{
-    public class ElfFuelRequierments
+    public class ElfFuelRequirement
     {
         public static void Main(string [] args){
             var masses = ReadMasses();
             var fuelRequirement = GetFuelRequirementPartOne(masses);
-            var totalFuelRequierment = GetFuelRequirementPartTwo(masses);
+            var totalFuelRequirement = GetFuelRequirementPartTwo(masses);
             //The elves fuel requirement is: 3432671, however the total fuel needed to carry fuel is: 5146132
-            Console.WriteLine($"The elves fuel requirement is: {fuelRequirement}, however the total fuel needed to carry fuel is: {totalFuelRequierment}");
+            Console.WriteLine($"The elves fuel requirement is: {fuelRequirement}, however the total fuel needed to carry fuel is: {totalFuelRequirement}");
         }
 
         private static int GetFuelRequirementPartOne(int[] masses)
@@ -20,7 +20,7 @@ namespace DayOne{
 
         private static int GetFuelRequirementPartTwo(int[] masses)
         {
-            return masses.Sum(mass => GetTotalFuelRequirement(mass));
+            return masses.Sum(GetTotalFuelRequirement);
         }
 
 
@@ -32,8 +32,11 @@ namespace DayOne{
 
         }
 
-        private static int[] ReadMasses(){
-            var masses = File.ReadLines("ElvesShipMasses.txt").Select(mass => Convert.ToInt32(mass)).ToArray();
+        private static int[] ReadMasses()
+        {
+            string projectDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"..\\..\\..\\"));
+            var dataDirectory = Path.Combine(projectDirectory, @"data", "ElvesShipMasses.txt");
+            var masses = File.ReadLines(dataDirectory).Select(mass => Convert.ToInt32(mass)).ToArray();
             return masses;
         }
     }
