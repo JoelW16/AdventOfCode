5aeceae2540bc1e1774e95b97db56a0654e116eb
Walker, Joel <11062267+JoelW16@users.noreply.github.com>
2019-12-04 13:44:44 +0000

    Day 4 part 1 and 2, find number of possible passwords

---
+++ DayFour/SecureContainer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace DayFour
+{
+    public class SecureContainer
+    {
+        
+        public static void Main(string[] args)
+        {
+            var numberOfPasswords = GetNumberOfPasswordsMatchesInRange(193651, 649729);
+            Console.WriteLine($"Passwords which match criteria: {numberOfPasswords}");
+        }
+
+        private static int GetNumberOfPasswordsMatchesInRange(int lower, int upper)
+        {
+            var possiblePasswords = new List<int>();
+            for (var i = lower; i < upper; i++)
+            {
+                var password = i.ToString();
+                if (IsSorted(password) && HasValidAdjacentDigits(password))
+                {
+                    possiblePasswords.Add(i);
+                }
+            }
+            return possiblePasswords.Count;
+        }
+
+        private static bool IsSorted(string password)
+        {
+            return Regex.IsMatch(password, @"^(0*1*2*3*4*5*6*7*8*9*)$");
+        }
+
+        private static bool HasValidAdjacentDigits(string password)
+        {
+            var adjacentDigits = new List<(char digit, int count)>();
+
+            foreach (var unit in password)
+            {
+                if (adjacentDigits.Count > 0 && adjacentDigits[^1].digit == unit)
+                {
+                    var lastAdjacent = adjacentDigits[^1];
+                    lastAdjacent.count++;
+                    adjacentDigits[^1] = lastAdjacent;
+                }
+                else
+                {
+                    adjacentDigits.Add((unit, 1));
+                }
+            }
+            //Part 1
+            //return adjacentDigits.Count(c => c.count > 1) > 0;
+
+            //Part 2
+            return adjacentDigits.Count(c => c.count == 2) > 0;
+        }
+
+    }
+}
